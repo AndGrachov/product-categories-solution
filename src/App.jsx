e3c784a35facccbe50c99355225322daa20254cc
AndGrachov <grachovand@gmail.com>
2024-08-01 12:49:29 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -108,36 +108,6 @@ export const App = () => {
                 activeCategory={activeCategory}
                 setCategory={setActiveCategory}
               />
-              {/* <a
-                href="#/"
-                data-cy="AllCategories"
-                className="button is-success mr-6 is-outlined"
-              >
-                All
-              </a>
-
-              <a
-                data-cy="Category"
-                className="button mr-2 my-1 is-info"
-                href="#/"
-              >
-                Category 1
-              </a>
-
-              <a data-cy="Category" className="button mr-2 my-1" href="#/">
-                Category 2
-              </a>
-
-              <a
-                data-cy="Category"
-                className="button mr-2 my-1 is-info"
-                href="#/"
-              >
-                Category 3
-              </a>
-              <a data-cy="Category" className="button mr-2 my-1" href="#/">
-                Category 4
-              </a> */}
             </div>
 
             <div className="panel-block">
